ea70c6e6ab0917608bb776299a9d690b93956f78
Terrrett <petroszamagias@hotmail.com>
2025-01-03 15:38:01 -0500

    Patched splashdown sat badge (#2007)
    * Splashdown Saturday badge :)
    
    * Splashdown Saturday fixed
    
    ---------
    
    Co-authored-by: Kalle <38327916+Sendouc@users.noreply.github.com>

--- app/features/badges/homemade.ts
+++ app/features/badges/homemade.ts
@@ -455,6 +455,11 @@ export const homemadeBadges: BadgeInfo[] = [
 		authorDiscordId: "336683473714544641",
 	},
 	{
+		displayName: "Splashdown Saturday",
+		fileName: "splashdownsat",
+		authorDiscordId: "631246535560265749",
+	},
+	{
 		displayName: "Low Tier Bouts",
 		fileName: "low-tier-bouts",
 		authorDiscordId: "430487372015206410",
